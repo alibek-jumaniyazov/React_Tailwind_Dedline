1209a6b7216bc89f2508cd1cc326e22a35d7a3ae
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-17 00:18:38 +0500

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import Main from "../components/main/Main";
 
 export default function Home() {
   return (
-    <div className='Home'>
-
+    <div className="Home">
+      <Header />
+      <Main />
+      <Footer />
     </div>
-  )
+  );
 }
