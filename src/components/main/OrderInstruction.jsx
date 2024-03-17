66944f513712fbd71a6529307df2ec5e43473ba1
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-17 23:30:07 +0500

    add

--- src/components/main/OrderInstruction.jsx
+++ src/components/main/OrderInstruction.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import delivery from '../../assets/icons/delivery.svg'
+import whiteDocIcon from '../../assets/icons/whiteDocIcon.svg'
+import taskList from '../../assets/icons/taskList.svg'
+
 
 export default function OrderInstruction() {
   return (
@@ -8,6 +12,29 @@ export default function OrderInstruction() {
         <p className=" text-[#E94B67]">раз</p>
         <p className=" text-[#F7CE46]">два</p>
         <p className=" text-[#0000B0]">три</p>
+      </div>
+      <div className="flex justify-center items-start gap-44 ml-24">
+        <div className="">
+          <div className="">
+            <img src={taskList} alt="" />
+          </div>
+          <p className="">Оставьте заявку по телефону</p>
+          <p className="">8 (800) 777-59-19</p>
+          <span className="">либо оформите заказ на сайте</span>
+        </div>
+        <div className="">
+          <div className="">
+            <img src={whiteDocIcon} alt="" />
+          </div>
+          <p className="">Подтвердите заказ у менеджера</p>
+        </div>
+        <div className="">
+            <div className="">
+              <img src={delivery} alt="" />
+            </div>
+            <p>Получите товар курьерской доставкой</p>
+            <span>или в нашем офисе: Варшавское шоссе 35</span>
+        </div>
       </div>
     </div>
   );
