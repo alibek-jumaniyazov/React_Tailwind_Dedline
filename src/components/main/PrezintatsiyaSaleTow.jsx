4a8a1b21a24c88bcc8b3ce97f50aafa1da5be530
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-17 19:26:39 +0500

    add

--- src/components/main/PrezintatsiyaSaleTow.jsx
+++ src/components/main/PrezintatsiyaSaleTow.jsx
@@ -5,13 +5,18 @@ export default function PrezintatsiyaSaleTow() {
   return (
     <div className="PrezintatsiyaSale w-[830px] h-[410px] bg-[#00B6BA] flex justify-between items-center p-5">
       <div className="h-full flex flex-col justify-between items-start fontMedium text-white w-64 text-5xl">
-        <h1>давайте займемся подарками!</h1>
+        <div className="flex flex-col justify-center items-start">
+          <div className="text-xs border border-white rounded-md p-1">
+            <p>корпоративные подарки</p>
+          </div>
+          <h1>давайте займемся подарками!</h1>
+        </div>
         <div className="flex gap-5">
           <button className="w-40 h-11 rounded-md border border-white fontMedium text-white text-sm">
-          Подробнее
+            Подробнее
           </button>
           <button className="w-40 h-11 rounded-md border border-white fontMedium text-white text-sm">
-          заказать
+            заказать
           </button>
         </div>
       </div>
