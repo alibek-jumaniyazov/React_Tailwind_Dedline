4c0206ac3b6b32c260c43a31b1a3010d219c5629
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-17 19:23:35 +0500

    add completes info

--- src/components/main/ProductAboutTow.jsx
+++ src/components/main/ProductAboutTow.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ArrowButton from "../ui/ArrowButton";
-import katalogImg1 from "../../assets/images/katalog1.png";
-import katalogImg2 from "../../assets/images/katalogBook.png";
-import katalogImg3 from "../../assets/images/katalogWashing.png";
+import katalogImg1 from "../../assets/images/prImg1.png";
+import katalogImg2 from "../../assets/images/prImg2.png";
+import katalogImg3 from "../../assets/images/prImg3.png";
 import PrezintatsiyaSaleTow from "./PrezintatsiyaSaleTow";
 
 
@@ -13,22 +13,22 @@ export default function ProductAboutTow() {
       <div className="flex justify-between items-center">
       <PrezintatsiyaSaleTow/>
 
-        <div className=" flex justify-center items-center gap-4">
-          <div className="group cursor-pointer w-80 h-[410px] flex flex-col justify-between items-center gap-3 ">
+        <div className="w-2/4 flex flex-col justify-center items-center gap-4">
+          <div className="group cursor-pointer w-80 h-[169px] flex flex-col justify-between items-center gap-3 ">
             <div className="">
               <p className="hidden group-hover:flex fontRoman">
                 Перейти в каталог
               </p>
             </div>
-            <img src={katalogImg1} alt="" className="w-56 h-56" />
+            <img src={katalogImg1} alt="" className="" />
             <div className=" w-full flex justify-end">
               <div className="hidden group-hover:flex">
                 <ArrowButton />
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-center items-start w-[200px] gap-3">
-            <div className="group cursor-pointer w-52 h-52  flex flex-col justify-center items-center p-5">
+          <div className="flex justify-center items-start w-full gap-3">
+            <div className="group cursor-pointer w-[340px] h-40  flex flex-col justify-center items-center p-5">
               <p className="hidden group-hover:flex fontRoman">
                 Перейти в каталог
               </p>
@@ -39,7 +39,7 @@ export default function ProductAboutTow() {
                 </div>
               </div>
             </div>
-            <div className="group cursor-pointer w-52 h-52  flex flex-col justify-center items-center">
+            <div className="group cursor-pointer w-[340px] h-40  flex flex-col justify-center items-center">
               <p className="hidden group-hover:flex fontRoman">
                 Перейти в каталог
               </p>
