d7f712f18ed10629379ff94deb6c1ac5d015bc56
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-17 16:12:51 +0500

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import footerIcon from '../../assets/icons/footerIcon.svg'
 
 export default function Footer() {
   return (
-    <div className='Footer'>
-<h1>Footer</h1>
+    <div className='Footer bg-[#090909] w-full p-7 fontRoman'>
+        <div className="flex w-full justify-between items-center">
+          <div className="flex flex-col justify-center items-start gap-7">
+            <span className='text-sm text-[#838383]'>По всем вопросам</span>
+            <p className='text-5xl text-[#F2F2F2] decoration-solid '>maldex@info.com</p>
+          </div>
+          <div className="flex flex-col justify-center items-start gap-7">
+            <span className='text-sm text-[#838383]'>Бесплатный звонок из любой точки России</span>
+            <p className='text-5xl text-[#F2F2F2] decoration-solid'>8-800 333-67-84</p>
+          </div>
+          <img src={footerIcon} alt="" />
+        </div>
+        <div className=""></div>
     </div>
   )
 }
