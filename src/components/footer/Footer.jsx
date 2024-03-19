d553502a33b0c78386266b2b85518b702668bbc8
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-03-20 03:20:16 +0500

    asd

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import FooterBottom from "./FooterBottom";
 
 export default function Footer() {
   return (
-    <div className="Footer flex flex-col justify-center items-start gap-14 bg-[#090909] w-full p-7 fontRoman">
+    <div className="Footer  flex flex-col justify-center items-start gap-14 bg-[#090909] w-full p-7 fontRoman max-[320px]:bg-red-700 flex flex-row gap-96"  >
       <FooterHead />
       <FooterMain />
       <FooterBottom />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens:{
+      "tablet":"430px",
+    },
     extend: {},
   },
   plugins: [],
